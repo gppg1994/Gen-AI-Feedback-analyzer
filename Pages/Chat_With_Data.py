4cb1690d6dd245368f08de48777f4b5302f636e2
Gaurav Paul <gppg1994@yahoo.com>
2024-05-30 19:09:41 +0530

    Create Chat_With_Data.py

---
+++ Pages/Chat_With_Data.py
@@ -0,0 +1,112 @@
+import pandas as pd
+from langchain_openai import AzureChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_experimental.tools import PythonREPLTool
+from langchain_experimental.agents import create_pandas_dataframe_agent
+import json
+import streamlit as st
+import time
+from langchain.agents import AgentType
+from langchain.prompts import MessagesPlaceholder
+from langchain.memory import ConversationBufferMemory
+from langchain.agents import initialize_agent, Tool
+import hydralit_components as hc
+import os,glob
+
+
+OPENAI_API_KEY = "4f4ffeb62271468f9ab3586cfe712d02"
+OPENAI_DEPLOYMENT_NAME = "deploy-gpt4"
+OPENAI_EMBEDDING_MODEL_NAME = "EmbeddingModel"
+MODEL_NAME = "gpt-35-turbo"
+# openai.api_type = "azure"
+AZURE_OPENAI_ENDPOINT = "https://testpoc123.openai.azure.com/"
+AZURE_OPENAI_API_VERSION = "2024-02-15-preview"
+
+#For including chat history refer: https://python.langchain.com/v0.1/docs/modules/agents/how_to/custom_agent/
+
+def stream_data(text):
+    for letter in text:
+        yield letter
+        time.sleep(0.02)
+        
+def chat(agent,chat_message):
+    cwd=os.getcwd()
+    if 'messages' in st.session_state:
+        for message in st.session_state.messages:
+            with st.chat_message(name='user'):
+                st.markdown(f"<span style='float:right;'>{message['input']}</span><br>",unsafe_allow_html=True)
+            with st.chat_message(name='ai'):
+                st.markdown(f"{message['output']}")
+    
+    if chat_message is not None:
+        try:
+            
+            with st.chat_message(name='user'):
+                st.write(f"<span style='float:right;'>{chat_message}</span><br>",unsafe_allow_html=True)
+            with st.spinner("Generating response..."):
+            
+                ai_msg=agent.invoke(
+                    {
+                        "input": chat_message
+                    }
+                )
+            if 'messages' in st.session_state:
+                st.session_state.messages.append(ai_msg)
+            else:
+                st.session_state.messages=[]
+                st.session_state.messages.append(ai_msg)
+
+            #st.write(f"<span style='float:right'>{input_msg}</span>",unsafe_allow_html=True)
+            with st.chat_message(name='ai'):
+                st.write(stream_data(ai_msg['output']))
+                #st.write(f"{stream_data(ai_msg['output'])}")
+            #st.write("</br>",unsafe_allow_html=True)
+                
+
+        except Exception as ex:
+            print(str(ex))
+            with st.chat_message(name='ai'):
+                st.write("Please rephrase your question and ask again")
+            ai_msg={
+                
+                    "input": chat_message,
+                    "output":"Please rephrase your question and ask again."
+                    
+            }
+            if 'messages' in st.session_state:
+                st.session_state.messages.append(ai_msg)
+            else:
+                st.session_state.messages=[]
+                st.session_state.messages.append(ai_msg)
+    if "messages" not in st.session_state : 
+        st.markdown("<div style='margin-left:30%;margin-top:20%'> <h5> ✨ Chat with your data</span><h5>",unsafe_allow_html=True)
+        #input_msg=st.chat_input("Ask anything about the data...")
+    # Display chat messages from history on app rerun
+
+        
+
+def main():
+        
+    st.set_page_config(layout="centered")
+
+    st.markdown("<h3>Insightify</h3>",unsafe_allow_html=True)
+
+    if 'openai_client' not in st.session_state:    
+        llm= AzureChatOpenAI(
+            azure_endpoint=AZURE_OPENAI_ENDPOINT,
+            api_key=OPENAI_API_KEY,
+            azure_deployment=OPENAI_DEPLOYMENT_NAME,
+            api_version=AZURE_OPENAI_API_VERSION
+        )
+        st.session_state.openai_client=llm
+    tools=[PythonREPLTool()]
+    if 'data' not in st.session_state:
+        st.toast("No data loaded. Redirecting to Home page...")
+        st.switch_page('Dashboard.py')
+    data=st.session_state.data
+    agent = create_pandas_dataframe_agent(st.session_state.openai_client,df=data,agent_type=AgentType.OPENAI_FUNCTIONS,prefix=f'''You are a data analyst.You may need to provide analysis of the data that has been  provided. You may also be asked intelligent insights that you can draw from the data. Answer the question that has been asked, articulately.''',extra_tools=tools,verbose=True)
+    input_msg=st.chat_input("Ask anything about the data...")
+    chat(agent=agent,chat_message=input_msg)
+
+if __name__=='__main__':
+    main()
