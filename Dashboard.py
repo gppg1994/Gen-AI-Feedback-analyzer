1c1092e8d8bfd220b9be867a7a613d18c343bc2c
Gaurav Paul <gppg1994@yahoo.com>
2024-06-03 23:30:28 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -63,7 +63,7 @@ def getResponse(_query):
 def generate_smart_response(_data):
     if 'key' not in st.session_state:
                 with st.spinner("Loading..."):
-                    intg_resp=getResponse(_data[['Timestamp','Feedback Summary','Department','Category','Sentiment','Source of feedback','Age','Location']])
+                    intg_resp=getResponse(_data[['Timestamp','Feedback','Feedback Summary','Department','Category','Sentiment','Source of feedback','Age','Location']])
                 
                     #intg_resp=getResponse(data)
                     st.session_state["key"]=intg_resp
