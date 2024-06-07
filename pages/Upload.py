aea24ec4f3808ba2c370e0bfc5276c3f403618a0
Gaurav Paul <gppg1994@yahoo.com>
2024-06-07 14:58:58 +0530

    Update Upload.py

--- pages/Upload.py
+++ pages/Upload.py
@@ -8,13 +8,12 @@ import datetime as dt
 import os
 
 OPENAI_API_KEY = "4f4ffeb62271468f9ab3586cfe712d02"
-OPENAI_DEPLOYMENT_NAME = "GetJobDescDetails"
+OPENAI_DEPLOYMENT_NAME = "deploy-gpt4"
 OPENAI_EMBEDDING_MODEL_NAME = "EmbeddingModel"
 MODEL_NAME = "gpt-35-turbo"
 # openai.api_type = "azure"
 AZURE_OPENAI_ENDPOINT = "https://testpoc123.openai.azure.com/"
-AZURE_OPENAI_API_VERSION = "2023-07-01-preview"
-# openai.api_key = OPENAI_API_KEY
+AZURE_OPENAI_API_VERSION = "2024-02-15-preview"
 
 client = AzureOpenAI(
     api_key=OPENAI_API_KEY,  
