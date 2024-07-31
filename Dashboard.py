7c66946d49d7832dc87a043e02470f216918a269
Gaurav Paul <gppg1994@yahoo.com>
2024-07-31 17:45:39 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -20,13 +20,11 @@ from langchain_experimental.agents import create_pandas_dataframe_agent
 
 
 
-OPENAI_API_KEY = "4f4ffeb62271468f9ab3586cfe712d02"
-OPENAI_DEPLOYMENT_NAME = "deploy-gpt4"
+OPENAI_API_KEY = "f29d9eef53fd451388bd7b1bc1a062a7"
+OPENAI_DEPLOYMENT_NAME = "GPT4-Turbo"
 OPENAI_EMBEDDING_MODEL_NAME = "EmbeddingModel"
-MODEL_NAME = "gpt-35-turbo"
-# openai.api_type = "azure"
-AZURE_OPENAI_ENDPOINT = "https://testpoc123.openai.azure.com/"
-AZURE_OPENAI_API_VERSION = "2024-02-01"
+AZURE_OPENAI_ENDPOINT = "https://deployment-gpt4.openai.azure.com/"
+AZURE_OPENAI_API_VERSION = "2024-05-01-preview"
 # openai.api_key = OPENAI_API_KEY
 
 client = AzureOpenAI(
